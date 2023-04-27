75b4a48da1c302f70007eabf736ae4714604c090
ticopiccirillo <121138268+ticopiccirillo@users.noreply.github.com>
2023-04-27 17:42:26 -0300

    Varias alterações

--- javascript/js.js
+++ javascript/js.js
@@ -1,13 +1,39 @@
-window.alert('Seja muito bem vindo!!')
-
 const btnMobile = document.getElementById('btn-mobile') ;
-btnMobile.addEventListener('click' ,toggleMenu) ;
+btnMobile.addEventListener('click', toggleMenu) ;
 btnMobile.addEventListener('Touchstart', toggleMenu) ;
 
-function toggleMenu( event) 
+function toggleMenu(event) 
 {
-    if(event.type === 'touchstart') eventpreventdefaul
+    if(event.type === 'touchstart') event.preventDefaul;
 const nav = document.getElementById('nav') ;
-nav.classList.toggle('active' )
-
+nav.classList.toggle('active')
 }
+
+
+//Funções scroll
+const menuItens = document.querySelectorAll('#menu a');
+
+menuItens.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+    const id = element.getAttribute('href');
+    return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+    event.preventDefault();
+    const to = getScrollTopByHref(event.target) - 100;
+
+    scrollToPosition(to);
+    }
+
+function scrollToPosition(){
+    windouw.scroll({
+        top: to,
+        behavior: "smooth",
+    });
+};
+
+
